ef1111b8e854bd2fc8bdd773d49e3cdb0037859b
laisla.barbosa <laisla.barbosa@gmail.com>
2024-11-03 19:10:53 -0300

    fix: middleware for protect routes

--- projetopi_frontend/src/middleware.ts
+++ projetopi_frontend/src/middleware.ts
@@ -1,29 +1,7 @@
-
-// import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware'
-// import { NextRequest } from 'next/server'
-
-// export default async function middleware(req: NextRequest) {
-//     return withAuth(req)
-// }
-
-// // Routes Middleware should not run on
-// export const config = {
-//     matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)', '/cadastros-publicos', '/criar-evento', '/eventos', '/login'],
-// }
-// ==============================
-
-
-
-// export { auth as middleware } from "./auth"
-
-
-
-// ==============================
 import { NextRequest, NextResponse } from 'next/server'
-import { decrypt } from '../auth'
+import { decrypt } from '@/actions/sessions'
 import { cookies } from 'next/headers'
 
-// 1. Specify protected and public routes
 const protectedRoutes = ['/dashboard']
 const publicRoutes = ['/', '/cadastros-publicos', '/criar-evento', '/eventos', '/login']
 
@@ -32,7 +10,7 @@ export default async function middleware(req: NextRequest) {
     // 2. Check if the current route is protected or public
     const path = req.nextUrl.pathname
     const isProtectedRoute = protectedRoutes.includes(path)
-    const isPublicRoute = publicRoutes.includes(path)
+    // const isPublicRoute = publicRoutes.includes(path)
 
     // 3. Decrypt the session from the cookie
     const cookie = (await cookies()).get('session')?.value
